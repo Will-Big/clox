4963d404d247c99649eb802091f5690c0d8c8a25
Sanghak Im <im.willbig@gmail.com>
2025-09-23 16:40:14 +0900

    memory allocator

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+#include "common.h"
+#include "chunk.h"
 
-int main(void)
+int main(int argc, const char* argv[])
 {
-    printf("Hello, World!\n");
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
+
     return 0;
 }
