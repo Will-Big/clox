444ea4a74112debfc90409b1c57513b3bdd1e017
Sanghak Im <im.willbig@gmail.com>
2025-09-25 21:12:31 +0900

    constant array & line debugger

--- main.c
+++ main.c
@@ -6,7 +6,10 @@ int main(int argc, const char* argv[])
 {
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN);
+    int constant = addConstant(&chunk, 1.2); // add constant to constant-pool
+    writeChunk(&chunk, OP_CONSTANT, 123); // write constant op-code
+    writeChunk(&chunk, constant, 123);    // write constant value
+    writeChunk(&chunk, OP_RETURN, 123);   // write return
     disassembleChunk(&chunk, "Test Chunk");
     freeChunk(&chunk);
 
